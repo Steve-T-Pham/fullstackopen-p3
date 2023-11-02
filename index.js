8e5ae67f02d349286bff57d7e144cbd7b9ce63b9
unknown <stp40@case.edu>
2023-11-01 20:05:35 -0400

    part3 completion

--- index.js
+++ index.js
@@ -1,84 +1,126 @@
-const express = require('express');
 require('dotenv').config()
-const morgan = require('morgan');
-const cors = require('cors');
-const app = express();
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+//schema from person.js
+const Person = require('./models/person')
 
+const requestLogger = (req, res, next) => {
+	console.log('Method:', req.method)
+	console.log('Path:', req.path)
+	console.log('Body:', req.body)
+	console.log('---')
+	next()
+}
 
-app.use(cors());
-app.use(express.json());
-app.use(express.static('dist'));
+const errorHandler = (err, req, res, next) => {
+	console.log(err)
 
+	if (err.name === 'CastError') {
+		return res.status(400).send({ error: 'malformatted id' })
+	}
+	else if (err.name === 'ValidationError') {
+		return res.status(400).json({ err: err.message })
+	}
+	else if (err.name === 'ValidatorError') {
+		return res.status(400).json({ err: err.message })
+	}
+
+	next(err)
+}
+
+const unknownEndpoint = (req, res) => {
+	res.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(cors())
+app.use(express.json())
+app.use(requestLogger)
+app.use(express.static('dist'))
+
+/*
 let persons = [
-    { 
+    {
       "id": 1,
-      "name": "Arto Hellas", 
+      "name": "Arto Hellas",
       "number": "040-123456"
     },
-    { 
+    {
       "id": 2,
-      "name": "Ada Lovelace", 
+      "name": "Ada Lovelace",
       "number": "39-44-5323523"
     },
-    { 
+    {
       "id": 3,
-      "name": "Dan Abramov", 
+      "name": "Dan Abramov",
       "number": "12-43-234345"
     },
-    { 
+    {
       "id": 4,
-      "name": "Mary Poppendieck", 
+      "name": "Mary Poppendieck",
       "number": "39-23-6423122"
     }
 ]
 
-morgan.token('content', (req, res) =>{
-  if (req.method === 'POST'){
-    return JSON.stringify(req.body);
-  }
-  return '-';
-});
-
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'));
-
 const generateId = () => {
   return(Math.floor(Math.random() * 1000));
 }
+*/
 
-app.get('/info', (req, res) =>{
-    const date = new Date();
-
-    res.send(
-        `<p>Phonebook has info for ${persons.length} people</p>
-        <p>${date}</p>`);
+morgan.token('content', (req) => {
+	if (req.method === 'POST'){
+		return JSON.stringify(req.body)
+	}
+	return '-'
 })
 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
+
+//info page
+app.get('/info', async (req, res) => {
+	const date = new Date()
+
+	res.send(
+		`<p>Phonebook has info for ${await Person.countDocuments({})} people</p>
+        <p>${date}</p>`)
+})
+
+//returns list of entries in database
 app.get('/api/persons', (req, res) => {
-    res.json(persons);
+	Person.find({}).then(person => {
+		res.json(person)
+	})
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const person = persons.find(person => person.id === id);
-
-    if (person) {
-        res.json(person);
-    }
-    else {
-        res.status(404).end();
-    }
+//returns specific entry related to id page
+app.get('/api/persons/:id', (req, res, next) => {
+	Person.findById(req.params.id).then(person => {
+		if (person){
+			res.json(person)
+		} else {
+			res.status(404).end()
+		}
+	})
+		.catch(err => {
+			next(err)
+		})
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    persons = persons.filter(person => person.id != id);
-
-    res.status(204).end();
+//delete function
+app.delete('/api/persons/:id', (req, res, next) => {
+	Person.findByIdAndRemove(req.params.id)
+		.then(() => {
+			res.status(204).end()
+		})
+		.catch(err => next(err))
 })
 
-app.post('/api/persons', (req, res) => {
-  const body = req.body;
-  const isDuplicate = persons.find(person => person.name === body.name);
+//creates a new entry
+app.post('/api/persons', (req, res, next) => {
+	const { name, number } = req.body
+
+	/*const isDuplicate = persons.find(person => person.name === body.name);
 
   if (!body.name || !body.number){
     return res.status(400).json({
@@ -90,18 +132,35 @@ app.post('/api/persons', (req, res) => {
       error: 'name must be unique'
     })
   }
+  */
 
-  const person = {
-    name: body.name,
-    number: body.number,
-    id: generateId()
-  }
+	const person = new Person({
+		name: name,
+		number: number,
+	})
 
-  persons = persons.concat(person);
-  res.json(person);
+	person.save().then(savedPerson => {
+		res.json(savedPerson)
+	})
+		.catch(err => next(err))
 })
 
+//updates old entry
+app.put('/api/persons/:id', (req, res, next) => {
+	const { name, number } = req.body
+
+	Person.findByIdAndUpdate(req.params.id, { name, number }, { new: true, runValidators: true, context: 'query' })
+		.then(updatedPerson => {
+			res.json(updatedPerson)
+		})
+		.catch(err => next(err))
+})
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+	console.log(`Server running on port ${PORT}`)
 })
